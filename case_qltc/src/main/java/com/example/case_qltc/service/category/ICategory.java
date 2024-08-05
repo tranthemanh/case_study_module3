0c060baa549d29758e92c06e9caab40aec97a951
tranthemanh <tranmanhhuha@gmail.com>
2024-08-05 16:27:17 +0700

    update ICategory

--- case_qltc/src/main/java/com/example/case_qltc/service/category/ICategory.java
+++ case_qltc/src/main/java/com/example/case_qltc/service/category/ICategory.java
@@ -1,12 +1,10 @@
 package com.example.case_qltc.service.category;
 
 import com.example.case_qltc.model.Category;
-import com.example.case_qltc.service.IGenerateService;
 
 import java.util.List;
-import java.util.concurrent.CompletionException;
 
-public interface ICategory {
+public interface ICategory{
     public List<Category> getAllCategory();
 
     public boolean insertCategory(Category category) throws Exception;
